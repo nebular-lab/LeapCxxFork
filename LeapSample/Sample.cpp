cf3c5181a2c26b323a3e237280f569bab8c2fa41
hiranodaisuke <hiranodaisuke0721@gmail.com>
2021-12-07 10:43:43 +0900

    ファイル入出力関係を削除

--- LeapSample/Sample.cpp
+++ LeapSample/Sample.cpp
@@ -16,7 +16,6 @@
 #include "../LeapSDK/include/LeapC.h"
 #include <Springhead.h>
 #include <Framework/SprFWApp.h>
-#include "FWFileLoaderSample.h"
 #include "../src/Samples/SampleApp.h"
 extern "C" {
 #include "ExampleConnection.h"
@@ -147,64 +146,7 @@ void OnPointMappingChange(const LEAP_POINT_MAPPING_CHANGE_EVENT* change) {
 //    event->head_orientation.z);
 //}
 
-FWFileLoaderSample::FWFileLoaderSample() {
-#ifdef USE_SPRFILE
-    fileName = "./file/scene.spr";	// sprファイル
-#else
-    fileName = "./file/scene.x";		// xファイル
-#endif
-}
-void FWFileLoaderSample::Init(int argc, char* argv[]) {
-    CreateSdk();			// SDKの作成
-    UTRef<ImportIf> import = GetSdk()->GetFISdk()->CreateImport();
-    GetSdk()->LoadScene(fileName, import);			// ファイルのロード
-    GetSdk()->SaveScene("save.spr", import);		// ファイルのセーブテスト
 
-}
-
-void FWFileLoaderSample::InitCameraView() {
-    Vec3d pos = Vec3d(-0.978414, 11.5185, 24.4473);		// カメラ初期位置
-    GetCurrentWin()->GetTrackball()->SetPosition(pos);	// カメラ初期位置の設定
-}
-
-void FWFileLoaderSample::Reset() {
-    GetSdk()->Clear();
-    GetSdk()->LoadScene(fileName);
-    GetCurrentWin()->SetScene(GetSdk()->GetScene());
-}
-
-
-void FWFileLoaderSample::Keyboard(int key, int x, int y) {
-    switch (key) {
-    case ESC:
-    case 'q':
-        // アプリケーションの終了
-        exit(0);
-        break;
-    case 'r':
-        // ファイルの再読み込み
-        Reset();
-        break;
-    case 'w':
-        // カメラ位置の初期化
-        InitCameraView();
-        break;
-    case 'd':
-    {
-        // デバック表示
-        static bool bDebug = GetSdk()->GetDebugMode();
-        if (bDebug)	bDebug = false;
-        else		bDebug = true;
-        GetSdk()->SetDebugMode(bDebug);
-        DSTR << "Debug Mode " << bDebug << std::endl;
-        //DSTR << "CameraPosition" << std::endl;
-        //DSTR << GetCurrentWin()->GetTrackball()->GetPosition() << std::endl;
-    }
-    break;
-    default:
-        break;
-    }
-}
 
 
 class MyApp : public SampleApp {
@@ -1208,8 +1150,8 @@ public:
                         PHSolidPairForLCPIf* solidpair;
                         bool swaped;
                         int state;
-                        solidpair = GetPHScene()->GetSolidPair(fg_obj_slide[0][3][0],solid, swaped);
-                        state=solidpair->GetContactState(0, 0);
+                        //solidpair = GetPHScene()->GetSolidPair(fg_obj_slide[0][3][0],solid, swaped);
+                        //state=solidpair->GetContactState(0, 0);
                         clock_t start, end;
                         if (state == 1 || state == 2) {
                             start = clock();
